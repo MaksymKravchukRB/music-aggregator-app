d6576c2499f88d02860eec8bc2db235870ec8fb5
MaksymKravchukRB <kravchuk.maksym2@lll.kpi.ua>
2025-05-13 18:30:41 +0300

    Removed deprecated ui elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import AudioPlayer from "./components/AudioPlayer";
-import BandcampControls from "./components/BandcampControls";
 import LoginButton from "./components/LoginButton";
-import IframeLoader from "./components/IframeLoader";
 import SpotifySearchBar from "./components/SpotifySearchbar";
 import LiveHistory from "./components/LiveHistory";
 
@@ -22,8 +19,6 @@ function App() {
     <div style={{ maxWidth: 600, margin: "auto" }}>
       <h1>Unified Music Playback</h1>
       <LoginButton />
-      <AudioPlayer />
-      <BandcampControls onTrackPlayed={triggerHistoryRefresh} />
       <SpotifySearchBar onTrackPlayed={triggerHistoryRefresh} />
       <LiveHistory refreshTrigger={refreshKey} />
       <button onClick={clearHistory}>Clear History</button>
